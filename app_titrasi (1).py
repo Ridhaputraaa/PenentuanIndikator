02b2faf9d921968aa89ab1febed8b6faced905e5
Ridhaputraaa <mangeak590@gmail.com>
2026-06-03 17:32:44 +0700

    Update app_titrasi (1).py

--- app_titrasi (1).py
+++ app_titrasi (1).py
@@ -471,7 +471,7 @@ st.divider()
 st.markdown(
     """
     <div style='text-align:center; color:#999; font-size:.8em; margin-top:4px'>
-        🧪 Sistem Rekomendasi Indikator Titrasi &nbsp;|&nbsp; Kimia Analitik &nbsp;|&nbsp; Kelompok 8
+        🥇 Sistem Rekomendasi Indikator Titrasi &nbsp;|&nbsp; Kimia Analitik &nbsp;|&nbsp; Kelompok 8
     </div>
     """,
     unsafe_allow_html=True,
