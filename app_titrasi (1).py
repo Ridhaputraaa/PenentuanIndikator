2dbb63a42168da2d7b83ef5224ca16f3f14f39db
Ridhaputraaa <mangeak590@gmail.com>
2026-06-03 17:33:34 +0700

    Update app_titrasi (1).py

--- app_titrasi (1).py
+++ app_titrasi (1).py
@@ -5,7 +5,7 @@ import streamlit as st
 # ─────────────────────────────────────────────
 st.set_page_config(
     page_title="Sistem Rekomendasi Titrasi",
-    page_icon="🧪",
+    page_icon="🥇",
     layout="centered",
 )
 
